a452768a482720512256b2bd59f4349cc7e85c9a
chiaifeng-cyber <chiaifeng@gmail.com>
2025-12-30 02:38:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,8 @@ with r:
     with st.container(border=True):
         con_auto = (
             f"1. Target +/-{ts:.3f}, CPK {cpk_v:.2f}, Yield {yld_v:.2f}%.\n"
-            f"2. Use the RSS method for the spec. All calculated tolerances must meet Cpk ≥ 1.0."
+            f"2. Use the RSS method for the spec. All calculated tolerances must meet a minimum CPK of 1.0."
         )
         st.text_area("Conclusion", value=con_auto if wc_v > 0 else "", height=100, label_visibility="collapsed")
+
 
