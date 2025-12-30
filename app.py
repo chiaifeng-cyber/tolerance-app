0fa7b3bfec42a76afc718dcadebdbcb854385106
chiaifeng-cyber <chiaifeng@gmail.com>
2025-12-30 11:28:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def get_init_df():
     return pd.DataFrame([
         {COLS[0]: "PCB", COLS[1]: "1.33", COLS[2]: "a", COLS[3]: "Panel mark to unit mark", COLS[4]: 0.100},
         {COLS[0]: "PCB", COLS[1]: "1.33", COLS[2]: "b", COLS[3]: "Unit mark to soldering pad", COLS[4]: 0.100},
-        {COLS[0]: "SMT", COLS[1]: "1.0", COLS[2]: "c", COLS[3]: "SMT tolerance", COLS[4]: 0.150},
+        {COLS[0]: "SMT", COLS[1]: "1.0", COLS[2]: "c", COLS[3]: "Assembly process", COLS[4]: 0.150},
         {COLS[0]: "Connector", COLS[1]: "1.33", COLS[2]: "d", COLS[3]: "Connector housing (0.25/2)", COLS[4]: 0.125}
     ])
 
@@ -178,4 +178,5 @@ with r:
             f"2. Use the RSS method for the spec. All calculated tolerances must meet a minimum CPK of 1.0."
         )
         st.text_area("Conclusion", value=con_auto if wc_v > 0 else "", height=100, label_visibility="collapsed")
+
 
